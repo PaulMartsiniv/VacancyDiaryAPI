613d28810015d097f9485edadc17d079815089ef
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-02-14 10:33:13 +0200

    Initial commit

---
+++ src/main/java/test/vacancy/diary/api/VacancyDiaryApiApplication.java
@@ -0,0 +1,13 @@
+package test.vacancy.diary.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyDiaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyDiaryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/vacancy/diary/api/VacancyDiaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.vacancy.diary.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyDiaryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
